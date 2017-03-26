4154c16210c004bfa6baaa1cc3fa984630e7ab71
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-03-26 09:27:47 -0500

    Fixed filepath

--- input.py
+++ input.py
@@ -6,21 +6,13 @@ def input_filename():
     """
     Function to input the filename from the handle the input from the user
     """
-    while True:
-        input_val = input("Please enter option: ")
-        if input_val == "1":
-            filename = input("\nPlease enter the filename: ")
-            if not filename.endswith(".txt"):
-                print("\nPlease check the filename given. It needs to be a txt file.")
-            return filename
-        elif input_val == "2":
-            sys.exit()
-        else:
-            print("\nPlease enter valid option!\n")
+    filename = input("Please enter the filename (Eg: input1.txt): ")
+    if not filename.endswith(".txt"):
+        print("\nPlease check the filename given. It needs to be a txt file.")
+    return filename
 
 
 def read_from_file(filename):
-
     """
     Function to read the lines from the input file and create new Card object for each line read
     :rtype: List of Card objects
@@ -37,4 +29,3 @@ def read_from_file(filename):
             if cards is not None:
                 card_list.append(cards)
         return card_list
-
--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import set
 
 
 def main():
-    print("Welcome to the set game!\n\nPlease don't forget to move the input file into the 'code' folder!\n\nEnter 1 to Enter the filename (Eg: input1.txt) \nEnter 2 to Exit\n")
+    print("Welcome to the set game!\n\nPlease don't forget to move the input file into the 'code' folder!\n")
     filename = input.input_filename()
-    filepath = os.path.dirname(__file__) + "/" + filename
-    card_list = input.read_from_file(filepath)
+    card_list = input.read_from_file(filename)
     set_cards = set.check_combination(card_list)
     largest_disjoint_set = set.find_largest_disjoint_set(set_cards)
     print_output(set_cards, largest_disjoint_set)
